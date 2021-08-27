fbae2ff123c522bdfa8bb89e747d9c75ebf51abb
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-08-27 15:06:15 -0300

    ejercicio32

--- Intervalos de tiempo/Practica.cs
+++ Intervalos de tiempo/Practica.cs
@@ -16,9 +16,36 @@ namespace Intervalos_de_tiempo
 
             DateTime fin_año = Convert.ToDateTime("31/12/2021");
 
-            TimeSpan rango = fecha - fin_año;
+            TimeSpan rango = fin_año - fecha;
 
             Console.WriteLine("La cantida de días que faltan hasta el próximo fin de año es de {0}", rango.Days);
+        }
+
+        public void ejercicio32()
+        {
+            Console.WriteLine("Ingrese la primer fecha por teclado");
+            string ingreso1 = Console.ReadLine();
+            DateTime fecha1 = Convert.ToDateTime(ingreso1);
+
+            Console.WriteLine("\nIngrese la segunda fecha por teclado");
+            string ingreso2 = Console.ReadLine();
+            DateTime fecha2 = Convert.ToDateTime(ingreso2);
+            TimeSpan intervalo;
+
+            if (fecha1 > fecha2)
+            {
+
+                intervalo = fecha1 - fecha2;
+            }
+            else
+            {
+                intervalo = fecha2 - fecha1;
+            }
+
+            Console.WriteLine("\n INTERVALOS");
+            Console.WriteLine("{0} DÍAS", intervalo.TotalDays);
+            Console.WriteLine("{0} MESES", intervalo.TotalDays/30 );
+            Console.WriteLine("{0} AÑOS", intervalo.TotalDays / 360);
         }
     }
 }

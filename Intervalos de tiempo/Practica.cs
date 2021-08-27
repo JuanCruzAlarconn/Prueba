cc80befa060c13ac9af1d2cdc3a2aa776196f695
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-08-27 14:59:19 -0300

    ejercicio33

---
+++ Intervalos de tiempo/Practica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos_de_tiempo
+{
+    class Practica
+    {
+        public void ejercicio33()
+        {
+            Console.WriteLine("Ingrese una fecha por teclado");
+            string ingreso = Console.ReadLine();
+            DateTime fecha = Convert.ToDateTime(ingreso);
+
+            DateTime fin_año = Convert.ToDateTime("31/12/2021");
+
+            TimeSpan rango = fecha - fin_año;
+
+            Console.WriteLine("La cantida de días que faltan hasta el próximo fin de año es de {0}", rango.Days);
+        }
+    }
+}
---
+++ Intervalos de tiempo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos_de_tiempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practica ejercicio = new Practica();
+
+            ejercicio.ejercicio33();
+
+            Console.WriteLine("Ingrese una tecla para finalizar con la ejecución");
+            Console.ReadKey();
+        }
+    }
+}

dcef53358cd2e69aef09514cbb4ce7d92b8fed48
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-08-27 08:54:58 -0300

    ejercicio 4

---
+++ La Consola/Practica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_Consola
+{
+    class Practica
+    {
+        public void ejercicio4()
+        {
+            string ingreso;
+
+            do
+            {
+                Console.WriteLine("ingrese cadenas hasta que al ingresar la cadena fin se detendrá la ejecución del código");
+                ingreso = Console.ReadLine();
+
+                if (ingreso == "fin")
+                {
+                    Console.WriteLine("\n Ha ingresado la cadena fin por lo que finanlizará la ejecución del programa\n");
+
+                }
+            } while (ingreso != "fin");
+        }
+    }
+}
---
+++ La Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practica ejercicio = new Practica();
+
+
+            ejercicio.ejercicio4();
+
+            Console.WriteLine("\n Ingrese cualquier tecla para salir de la consola de ejecución\n");
+
+            Console.ReadKey();
+
+        }
+    }
+}

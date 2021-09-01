8821ba66869299ba3f914843b5d9052a4c0f6cc2
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-09-01 18:07:16 -0300

    ejercicio49

---
+++ Manejo de archivos/Practica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Manejo_de_archivos
+{
+    class Practica
+    {
+        public void ejercicio49()
+        {
+            string ruta = "I:\\CAI\\Manejo de archivos\\base de datos.txt";
+
+            StreamWriter escribir = new StreamWriter(ruta); //no hace falta tener que hacer comprobaciónes dado que si el archivo no esta disponible se generara uno con el mismo nombre dentro de la ruta de acceso
+
+            escribir.WriteLine("Hola mundo");
+
+            Console.WriteLine("\nSe ha generado el archivo con el texto solicitado satisfactoriamente");
+
+            escribir.Close();//Todo archivo que se abre debe de cerrarce dentro de la misma interacción por función especificada
+
+        }
+    }
+}
---
+++ Manejo de archivos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_de_archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practica ejercicio = new Practica();
+
+            Console.WriteLine("Ingrese una tecla para finalizar con la ejecución del recurso");
+        }
+    }
+}

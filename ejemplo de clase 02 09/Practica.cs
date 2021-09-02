99ced0830707a13bd4c59d83c4315dea665df6e5
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-09-02 19:50:12 -0300

    ejercicio de clase

---
+++ ejemplo de clase 02 09/Practica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_de_clase_02_09
+{
+    class Practica
+    {
+        //1. 2 campos para poder generar un lista
+        //2. la clave no se repite es única y con ella puedo tener acceso al campo correspondiente al diccionario
+
+        public void ejercicio1()
+        {
+            Dictionary<int, string> personas = new Dictionary<int, string>();
+
+            personas.Add(38456910, "Juan Cruz Alarcón");
+            personas.Add(45645681, "Juan Perez");
+
+            //más suma de datos
+
+            string nombre = personas[38456910];// busco por el nombre de la clave que debo de tener identificada y es única
+
+            foreach(string name in personas.Values)
+            {
+                //estoy recorriendo todos los valores del diccionario
+
+                Console.WriteLine(name);
+
+
+            }
+
+        }
+    }
+}
---
+++ ejemplo de clase 02 09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_de_clase_02_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practica ejercicio = new Practica();
+
+            ejercicio.ejercicio1();
+
+            Console.WriteLine("\nIngrese una tecla para finalizar con la ejecución del programa");
+
+            Console.ReadKey();
+        }
+    }
+}

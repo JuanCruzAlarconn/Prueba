756de5ef241dbfcaaf808463b1fd1b2b1195ace5
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-09-02 20:40:26 -0300

    ejercicio clase

--- ejemplo de clase 02 09/Persona.cs
+++ ejemplo de clase 02 09/Persona.cs
@@ -8,8 +8,16 @@ namespace ejemplo_de_clase_02_09
 {
     class Persona
     {
-        public string nombre { get; set; }
+
+        public static int cantidad { get; private set; }//no pertece a una instancia en particular sino que pertenece a la clase
+
+        public string nombre { get; set; }   //nombre y apellido son propiedades de instancias
 
         public string apellido { get; set; }
+
+        public Persona()
+        {
+            cantidad++;//en el momento de crear un objeto persona aumenta el contador
+        }
     }
 }

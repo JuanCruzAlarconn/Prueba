8cf35d383bd11bb813b77f2a5b256128454fb5c5
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-09-03 18:37:34 -0300

    ejercicio54

--- Ejercicios Integrales/Practica.cs
+++ Ejercicios Integrales/Practica.cs
@@ -8,6 +8,8 @@ namespace Ejercicios_Integrales
 {
     class Practica
     {
+        List<Alumno> lista_alumnos = new List<Alumno>(); //como estan por fuera de los metodos se puede acceder sin tener que hacer referencias de algún tipo dentro de los subprogramas
+        List<Curso> lista_curso = new List<Curso>();
         public void ejercicio54()
         {
             ingreso();
@@ -47,7 +49,7 @@ namespace Ejercicios_Integrales
                     asignar_alumno();//Tomo un alumno y un curso hago la asignación cruzada correspondiente
                     break;
                 case 'd':
-                    desasignar_alumno();//Tomo un alumno me arroja una cantidad de cursos y elijo a cual me quiero dar de baja
+                    desasignar_alumno();//Paso al alumno y al curso al que quiero desasignar
                     break;
                 case 'e':
                     ver_alumnos_asignados(); // Toma un curso y devuelve la lista de alumnos que tiene asignados
@@ -71,6 +73,236 @@ namespace Ejercicios_Integrales
 
 
             }
+        }
+
+        public void asignar_alumno()
+        {
+            string ingreso_registro = "";
+            bool flag = true;
+
+            do
+            {
+                Console.WriteLine("\nIngrese el número de registro del alumno que desea asignar");
+                ingreso_registro = Console.ReadLine();
+                flag = validar_asignacion(ingreso_registro);
+            } while (flag);
+
+            int registro = Convert.ToInt32(ingreso_registro);
+
+            Alumno a = lista_alumnos.Find(A => A.numero_registro == registro);
+
+            string ingreso_curso = "";
+            flag = true;
+            do
+            {
+                Console.WriteLine("\nIngrese el código del curso al cual desea asignar al alumno: ");
+                Console.WriteLine("NOMBRE: {0} REGISTRO: {1}", a.nombre_completo, a.numero_registro);
+
+                ingreso_curso = Console.ReadLine();
+                flag = validar_asignacion_curso(ingreso_curso);
+            } while (flag);
+
+            int codigo_curso = Convert.ToInt32(ingreso_curso);
+
+            Curso c = lista_curso.Find(C => C.codigo_curso == codigo_curso);//solamente te da una copia del elemento al que hice una incurrencia dentro del contexto de la lista de cursos
+
+            lista_alumnos = lista_alumnos.Select(A => { A.cursos_asignados.Add(c); return A; }).ToList(); //SINTAXIS PARA MODIFICAR LAS PROPIEDADES DE ELEMENTOS DENTRO UNA LISTA
+            lista_curso = lista_curso.Select(curso => { curso.lista_curso.Add(a); return curso; }).ToList();
+
+            Console.WriteLine("\nLa asignación de curso fue exitosa");
+
+        }
+
+        public bool validar_asignacion_curso(string ingreso)
+        {
+            bool flag = true;
+            if (string.IsNullOrEmpty(ingreso))
+            {
+                Console.WriteLine("\nEl numero de curso no puede ser vacio");
+            }
+            else if (!Int32.TryParse(ingreso, out int salida))
+            {
+                Console.WriteLine("\nDebe de ingresar un tipo numérico dentro del numero de curso");
+            }
+            else if (salida <= 0)
+            {
+                Console.WriteLine("\nEl número de curso debe de ser un número positivo");
+            }
+            else if (lista_curso.Find(C=>C.codigo_curso==salida)==null)
+            {
+                Console.WriteLine("\nEl curso introducido no se corresponde con ninguno de los cursos disponibles dentro de la base del sistema");
+            }
+            else
+            {
+                flag = false;
+            }
+
+            return flag;
+
+        }
+
+        public bool validar_asignacion(string ingreso)
+        {
+            bool flag = true;
+
+            if (string.IsNullOrEmpty(ingreso))
+            {
+                Console.WriteLine("\nNo puede ingresar un registro vacio");
+            }
+            else if (!Int32.TryParse(ingreso, out int salida))
+            {
+                Console.WriteLine("\nDebe de ingresar un registro numérico");
+            }
+            else if (salida<=0)
+            {
+                Console.WriteLine("\nEl número de registro debe de ser un número positivo");
+            }
+            else if (lista_alumnos.Find(A=>A.numero_registro==salida)==null)
+            {
+                Console.WriteLine("\nEl número de registro completado no se correponde con ninguno de los alumnos dentro de la lista del sistema");
+            }
+            else
+            {
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        public void alta_curso()
+        {
+            string ingreso = "";
+            bool flag = true;
+
+
+            do
+            {
+                Console.WriteLine("\nIngrese el código de curso que desea dar de alta");
+                ingreso = Console.ReadLine();
+                flag = validar_curso(ingreso);
+            } while (flag);
+
+            int numero_curso = Convert.ToInt32(ingreso);
+
+            Curso c = new Curso();
+
+            c.codigo_curso = numero_curso;
+            c.lista_curso = new List<Alumno>();
+
+            lista_curso.Add(c);
+
+            Console.WriteLine("\nSe ha agregado satisfactoriamente un nuevo curso dentro de la lista del programa en ejecución");
+        }
+
+        public bool validar_curso(string ingreso)
+        {
+            bool flag = true;
+            if (string.IsNullOrEmpty(ingreso))
+            {
+                Console.WriteLine("\nEl numero de curso no puede ser vacio");
+            }
+            else if (!Int32.TryParse(ingreso, out int salida))
+            {
+                Console.WriteLine("\nDebe de ingresar un tipo numérico dentro del numero de curso");
+            }
+            else if (salida <= 0)
+            {
+                Console.WriteLine("\nEl número de curso debe de ser un número positivo");
+            }
+            else if (!(lista_curso.Find(A => A.codigo_curso == salida) == null))
+            {
+                Console.WriteLine("\nEl número de curso que intenta ingresar ya se halla cargado dentro del sistema por lo que no puede cargar 2 numeros de curso iguales");
+            }
+            else
+            {
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        public void alta_alumno()
+        {
+            string ingreso_registro = "";
+            string ingreso_nombre = "";
+            bool flag = true;
+
+            do
+            {
+                Console.WriteLine("\nIngrese el número de registro del alumno que quiere ingresar dentro del sistema");
+                ingreso_registro = Console.ReadLine();
+                flag = validar_registro(ingreso_registro);
+            } while (flag);
+
+            flag = true;
+
+            do
+            {
+                Console.WriteLine("\nIngrese el nombre del alumno correspondiente al registro {0}", ingreso_registro);
+                ingreso_nombre = Console.ReadLine();
+                flag = validar_nombre(ingreso_nombre);
+            } while (flag);
+
+            int registro = Convert.ToInt32(ingreso_registro);
+            string nombre = ingreso_nombre;
+
+            Alumno A = new Alumno();
+
+            A.numero_registro = registro;
+            A.nombre_completo = nombre;
+            A.cursos_asignados = new List<Curso>(); //Unicamente hago la declaración que inicializa la listam, esta aún permanece vacia
+
+            lista_alumnos.Add(A);
+            Console.WriteLine("\nSe ha agregado satisfactoriamente un nuevo alumno dentro de la lista de alumnos del sistema");
+
+           
+        }
+
+        public bool validar_nombre(string ingreso)
+        {
+            bool flag = true;
+
+            if(string.IsNullOrEmpty(ingreso))
+            {
+                Console.WriteLine("\nEl campo de nombre no puede permanecer vacio para la carga");
+            }
+            else if (ingreso.Length<2)
+            {
+                Console.WriteLine("\nDebe de incluir en nombre y apellido completo del alumno dentro del campo");
+            }
+            else
+            {
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        public bool validar_registro(string ingreso)
+        {
+            bool flag = true;
+            if(string.IsNullOrEmpty(ingreso))
+            {
+                Console.WriteLine("\nEl numero de registro no puede ser vacio");
+            }
+            else if (!Int32.TryParse(ingreso, out int salida))
+            {
+                Console.WriteLine("\nDebe de ingresar un tipo numérico dentro del registro");
+            }
+            else if (salida<=0)
+            {
+                Console.WriteLine("\nEl registro debe de ser un número positivo");
+            }
+            else if (!(lista_alumnos.Find(A=>A.numero_registro==salida)==null))
+            {
+                Console.WriteLine("\nEl número de registro que intenta ingresar ya se halla cargado dentro del sistema por lo que no puede cargar 2 numeros de registros iguales");
+            }
+            else
+            {
+                flag = false;
+            }
+
+            return flag;
         }
 
         public  bool validar_ingreso(string ingreso, ref char opcion)

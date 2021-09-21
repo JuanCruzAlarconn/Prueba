81b65f37712f1de3665c52da4ea4c36368e1b5d9
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-09-21 19:37:41 -0300

    estrutura

--- Ejercicio estructura de datos/Numero_Telefono.cs
+++ Ejercicio estructura de datos/Numero_Telefono.cs
@@ -12,5 +12,36 @@ namespace Ejercicio_estructura_de_datos
         public int codigo_pais { get; set; }
         public int codigo_area { get; set; }
         public int numero_telefono { get; set; }
+
+
+        public static Numero_Telefono Crear()
+        {
+            Numero_Telefono numero = new Numero_Telefono();
+
+            numero.tipo = asignar_tipo();
+            numero.codigo_area = asignar_codigo("area");
+            numero.codigo_pais = asignar_codigo("pais");
+            numero.numero_telefono = asignar_telefono();
+
+            Console.WriteLine("\nSe ha creado un teléfono satisfactoriamente");
+
+            return numero;
+
+        }
+
+        private static int asignar_telefono()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int asignar_codigo(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string asignar_tipo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

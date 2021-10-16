6d4d32471d785f09e9d99afa5918345c657b456e
JUAN CRUZ ALARCÓN <94AL38456910@campus.economicas.uba.ar>
2021-10-16 09:27:34 -0300

    estregable

--- Desarrollo de clases/Orden de servicio.cs
+++ Desarrollo de clases/Orden de servicio.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Desarrollo_de_clases
 {
     class Orden_de_servicio
     {
-        public string estado { get; set; }
+       
         public int codigo { get; set; }
-        public DateTime fecha { get; set; }
+        public DateTime fecha_ingreso { get; set; }
+        public DateTime? fecha_egreso { get; set; } // Cuando nace el objeto permanece en null hasta que lo damos por finalizado, la clase transporte le da la fecha de fin
         public Paquete paquete { get; set; }
-        public int codigo_transporte_asignado { get; set; }
+        public int? codigo_transporte_asignado { get; set; }
+        public int? codigo_cliente { get; set; }
         public int codigo_seguro { get; set; }
         public decimal precio { get; set; }
         public Punto_geografico origen { get; set; }
@@ -20,25 +23,50 @@ namespace Desarrollo_de_clases
         public string modo_retiro { get; set; }
         public string modo_entrega { get; set; }
         public bool urgente { get; set; }
+        public int? codigo_agencia { get; set; }
 
         public static Orden_de_servicio Crear()
         {
             Orden_de_servicio orden_de_servicio = new Orden_de_servicio();
+            orden_de_servicio.codigo_cliente = asignar_cliente();
             orden_de_servicio.codigo = asignar_codigo();
-            orden_de_servicio.fecha = asignar_fecha();
-            orden_de_servicio.codigo_transporte_asignado = asignar_transporte();
+            orden_de_servicio.fecha_ingreso = asignar_fecha();
             orden_de_servicio.codigo_seguro = asignar_seguro();
             orden_de_servicio.origen = Punto_geografico.crear();
             orden_de_servicio.destino = Punto_geografico.crear();
+            orden_de_servicio.paquete = asignar_paquete();
+            orden_de_servicio.codigo_transporte_asignado = asignar_transporte(orden_de_servicio.codigo, orden_de_servicio.origen, orden_de_servicio.destino,orden_de_servicio.paquete);
             orden_de_servicio.modo_entrega = asignar_modo_entrega();
             orden_de_servicio.modo_retiro = asignar_modo_retiro();
             orden_de_servicio.urgente = asignar_urgencia();
+            orden_de_servicio.codigo_agencia = asignar_agencia();
 
             return orden_de_servicio;
 
 
 
 
+
+        }
+
+        private static int? asignar_agencia()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int? asignar_cliente()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Paquete asignar_paquete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string consultar_estado()
+        {
+            throw new NotImplementedException();
 
         }
 
@@ -62,9 +90,14 @@ namespace Desarrollo_de_clases
             throw new NotImplementedException();
         }
 
-        private static int asignar_transporte()
+        private static int? asignar_transporte(int codigo_servicio, Punto_geografico origen, Punto_geografico destino,Paquete paquete)
         {
-            throw new NotImplementedException();
+           
+
+            int? codigo_transporte = Transporte.ver_disponibilidad(codigo_servicio, origen,destino,paquete);
+            //si no hay posibilidad de poder asignarle un medio de transporte queda como null la asignación
+
+            return codigo_transporte;
         }
 
         private static DateTime asignar_fecha()
@@ -73,6 +106,25 @@ namespace Desarrollo_de_clases
         }
 
         private static int asignar_codigo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Orden_de_servicio> abrir_archivo()
+        {
+            var lista = new List<Orden_de_servicio>();
+
+
+            return lista;
+        }
+
+        public void modificar_archivo(List<Orden_de_servicio> lista)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void finalizar()
         {
             throw new NotImplementedException();
         }
---
+++ Desarrollo de clases/Transporte.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Desarrollo_de_clases
+{
+    class Transporte
+    {
+        public int codigo { get; set; }
+        public string categoria { get; set; }
+        public string estado { get; set; } //habla del estado de la unidad si puede seguir prestando servicio o debe de remitirse a mentenimiento
+        public int? codigo_orden_asignada { get; set; }
+        public decimal kilometros_recorridos_viaje { get; set; }
+
+       
+
+        public int? codigo_agencia { get; set; }//solo le es asignada en caso de que se necesite trasladar el paquete a domiciolio
+
+        public bool? fin_de_viaje { get; set; }
+
+        public static Transporte crear()
+        {
+            Transporte transporte = new Transporte();
+
+            transporte.codigo = asignar_codigo();
+            transporte.categoria = asignar_categoria();
+            transporte.estado = asignar_estado();
+            transporte.kilometros_recorridos_viaje = 0;
+
+            return transporte;
+
+
+        }
+
+        private static int asignar_codigo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string asignar_categoria()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string asignar_estado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void actualizar_estado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ejecutar_mantemiento()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void asignar_orden_de_servicio(int codigo_servicio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Transporte> abrir_archivo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int? ver_disponibilidad(int codigo_servicio,Punto_geografico origen, Punto_geografico destino, Paquete paquete)
+        {
+            //Evaluar si puede mandar un paquete de ese peso desde el punto de origen hacia el destino marcado
+            List<Transporte> lista_transporte = Transporte.abrir_archivo();  //Debe de haber una apertura de un archivo donde lo paso hacia una lista para poder trabajar el contenido
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+        public static void Modificar_archivo(List<Transporte> lista_transporte)
+        {
+            //Ingreso de una lista modificada que sobreescribe la lista preexistente dentro del archivo
+
+
+            throw new NotImplementedException();
+
+        }
+       
+    }
+}
